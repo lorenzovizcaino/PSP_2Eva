7f1b067a2072f34b84a871046865952bde4d66db
Antonio Lorenzo Vizcaino <lorenzovizcaino@gmail.com>
2023-01-18 21:25:54 +0100

    18Ene23_21:225

--- ejerciciosUd2/src/factorial/Factorial.java
+++ ejerciciosUd2/src/factorial/Factorial.java
@@ -2,8 +2,19 @@ package factorial;
 /*
 Hacer un programa que calcule el factorial. Cada uno de los calculos tiene que ser realizado por un hilo independiente.
 Implementar los procesos de 2 maneras diferentes:
-    a)Heredando de la calse Thread.
+    a)Heredando de la clase Thread.
     b)Implementando la interfaz Runnable
  */
 public class Factorial {
+    int numero;
+
+    public Factorial(int numero) {
+        this.numero = numero;
+    }
+    public int Calculos(int numero){
+        int resultado=0;
+
+
+        return resultado;
+    }
 }
