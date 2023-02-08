8025090641b8bdce9b849b045bc38b20a59f16a0
Antonio Lorenzo <lorenzovizcaino@gmail.com>
2023-02-08 12:57:16 +0100

    08Feb23_12:57

--- ejerciciosUd2/src/productorConsumidorApuntes/Consumidor.java
+++ ejerciciosUd2/src/productorConsumidorApuntes/Consumidor.java
@@ -14,7 +14,8 @@ public class Consumidor extends Thread{
         int valor=0;
         for (int i=1; i<=5; i++) {
             valor = reci.vaciar();
-            System.out.println("Consumidor "+numero+" toma el valor "+i);
+            System.out.println("Consumidor "+numero+" toma el valor "+valor);//cambie i por valor ya que debe imprimir el valor recibido del metodo,
+                                                                             //no el valor del bucle
 
             //espera un tiempo antes de volver a consumir
             try {
